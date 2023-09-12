18ed83dbf1b0f5a44b6c66df02c6015a123cb8ed
Alex Philip <alexphilip55555@gmail.com>
2023-09-12 01:00:11 +0000

    updated stack

--- scripts/service-discovery.sh
+++ scripts/service-discovery.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "Fetching the Public IP Address"
 pub_ip=$(curl wgetip.com)
-file="prometheus.yml"
-file2="targets.json"
+file="./prometheus/prometheus.yml"
+file2="./prometheus/targets.json"
 ip="localhost"
-printf "%s\nThe Public IP is: $pub_ip \n" 
+printf "%s\nThe Public IP is: $pub_ip \n"
 echo "Replacing $ip with $pub_ip in $file"
 sed -i "s/$ip/$pub_ip/g" "$file"
 echo "The Updated $file"
